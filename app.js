2ff209ffb684e9a50f23c1f73b146984765b0930
Edddo123 <edo.miraqian@gmail.com>
2021-05-19 02:39:11 +0400

    test db connection

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require("express");
 
 const app = express();
+const { Pool } = require("pg");
 
+const pool = new Pool({
+  user: "postgres",
+  password: "password",
+  host: "localhost",
+  port: 5432,
+  database: "dbName",
+  max: 10,
+  connectionTimeoutMillis: 0 //if pools are taken wait forever
+//   idleTimeoutMillis: 0 //if connection not used still hold it forever
+});
 
 app.get('/', (req, res, next)=> {
     res.send("<h1>Hey there</h1>")
+})
+
+app.get('/db', async(req, res, next) => {
+    const data = await pool.query(`SELECT * FROM buyer`)
+
+    res.json({message: "success", data: data.rows[0]});
 })
 
 
